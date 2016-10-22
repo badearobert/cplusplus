51ac1c93d9e8838ba4c7edc3252406a2c061fb60
badearobert <badea.robert@ymail.com>
2016-10-22 12:31:37 +0300

    Update FactoryPatternExample.hpp

--- FactoryPatternExample.hpp
+++ FactoryPatternExample.hpp
@@ -10,9 +10,9 @@
 * Factory method pattern is a creational pattern that uses factory methods to deal with the problem of creating objects 
 * without having to specify the exact class of the object that will be created. 
 * 
-* This is done by creating objects by calling a factory method—either specified in an interface 
+* This is done by creating objects by calling a factory methodâ€”either specified in an interface 
 * and implemented by child classes, or implemented in a base class and optionally overridden by 
-* derived classes—rather than by calling a constructor.
+* derived classesâ€”rather than by calling a constructor.
 * 
 * Source: https://en.wikipedia.org/wiki/Factory_method_pattern
 *****************************************************************************/
@@ -20,15 +20,15 @@
 //--------------------------------------------------------------------
 //                              Includes
 //--------------------------------------------------------------------
-#include <memory>				// for C++11 unique_ptr
-#include <string>				// param in factory pattern implementation
+#include <memory>		// for C++11 unique_ptr
+#include <string>		// param in factory pattern implementation
 
 namespace NDesignPatterns
 {
 	namespace NFactoryPattern
 	{
 		//------------------------------------------------------------------------
-		//						INIIALIZATION OF THE VARIABLES
+		//			INIIALIZATION OF THE VARIABLES
 		//------------------------------------------------------------------------
 
 		/*
@@ -36,8 +36,8 @@ namespace NDesignPatterns
 		* It cannot be instantiated, the call function need to be overloaded by the derived classes
 		*
 		* Note		The void received as parameter is purely visual. 
-		*			The class definition was implemented in the same hpp line, this is called inlining.
-		*			I've decided to have it this way so we have only one file for each pattern.
+		*		The class definition was implemented in the same hpp line, this is called inlining.
+		*		I've decided to have it this way so we have only one file for each pattern.
 		*		
 		*/
 		class CBaseClass
@@ -47,8 +47,8 @@ namespace NDesignPatterns
 
 		/*
 		* Note		The override keyword after method declaration, it was implemented starting with C++11.
-		*			Its purpose is to notify the reader that the function is overriding an inherited virtual function
-		*			The compiler will crash if override is used on a non virtual function
+		*		Its purpose is to notify the reader that the function is overriding an inherited virtual function
+		*		The compiler will crash if override is used on a non virtual function
 		*/
 		class CDerivedClassOne : public CBaseClass
 		{
@@ -61,10 +61,10 @@ namespace NDesignPatterns
 
 		/*
 		* Note:		This object is so called the NULL OBJECT PATTERN. 
-		*			It is used to mimic the functionality of a class by deriving the base class,
-		*			without creating any logic for its overloaded functions.
-		*			In our case, we use to return the reference of the object created if the factory method
-		*			did not enter any of the implemented cases.
+		*		It is used to mimic the functionality of a class by deriving the base class,
+		*		without creating any logic for its overloaded functions.
+		*		In our case, we use to return the reference of the object created if the factory method
+		*		did not enter any of the implemented cases.
 		*/
 		class CNullObject : public CBaseClass
 		{
@@ -72,16 +72,19 @@ namespace NDesignPatterns
 		};
 
 		//------------------------------------------------------------------------
-		//						FACTORY METHOD PATTERN - C++0x
+		//			FACTORY METHOD PATTERN IMPLEMENTATION
 		//------------------------------------------------------------------------
 		class CFactory
 		{
 		public:
+			//------------------------------------------------------------------------
+			//			 IMPLEMENTATION IN C++0X
+			//------------------------------------------------------------------------
 			/**
 			* Note		The function will return nullptr if we did not matched any cases.
-			*			When used with multiple values, the factory is most likely implemented using switch 
+			*		When used with multiple values, the factory is most likely implemented using switch 
 			*			
-			*			We can use CNullObject at the end to return a null object, or we can use nullptr (both valid)
+			*		We can use CNullObject at the end to return a null object, or we can use nullptr (both valid)
 			**/
 			static CBaseClass* CreateObject_Example1(std::string objectName)
 			{
@@ -95,10 +98,12 @@ namespace NDesignPatterns
 				}
 				return new CNullObject();
 			}
-
+			//------------------------------------------------------------------------
+			//			 IMPLEMENTATION IN C++11
+			//------------------------------------------------------------------------
 			/**
 			* Note		In C++11 we have a function that expects a unique_ptr container
-			*			We can either return a null object ptr, or return an unique_ptr without ownership
+			*		We can either return a null object ptr, or return an unique_ptr without ownership
 			**/
 			static std::unique_ptr<CBaseClass> CreateObject_Example2(std::string objectName)
 			{
