cd52ee62f511b4c093479b976c0078b67f9f7811
badearobert <badea.robert@ymail.com>
2016-11-07 20:10:11 +0200

    Update OperatorOverloadingExample.hpp

--- OperatorOverloadingExample.hpp
+++ OperatorOverloadingExample.hpp
@@ -4,7 +4,7 @@
 *
 * @file         OperatorOverloadingExample.hpp
 * @author       Robert Badea
-* @url			www.robertbadea.ro
+* @url		www.robertbadea.ro
 ******************************************************************************
 * Operators overloading
 *
@@ -23,7 +23,7 @@ namespace NUtility
 	namespace NOperatorOverloading
 	{
 		//------------------------------------------------------------------------
-		//			Base Class so we can have the data we want
+		//		Base Class so we can have the data we want
 		//------------------------------------------------------------------------
 		class CBase
 		{
@@ -45,15 +45,15 @@ namespace NUtility
 			/**
 			* Note		We have const on the parameter object, the reason is that we are not allowed to modify it!
 			*				
-			*			In order to support operator chaining (a = b = c), the assignment operator must return some value. 
-			*			The value that should be returned is a reference to the left-hand side of the assignment.
+			*		In order to support operator chaining (a = b = c), the assignment operator must return some value. 
+			*		The value that should be returned is a reference to the left-hand side of the assignment.
 			*
-			*			Always check for self-assignment!
+			*		Always check for self-assignment!
 			*
 			* Signature
-			*			MyClass & operator=(const MyClass &rhs)
+			*		MyClass & operator=(const MyClass &rhs)
 			*
-			*			The operator returns a non - const reference to this
+			*		The operator returns a non - const reference to this
 			*/
 			CCopyableExample & operator=(const CCopyableExample &rhs)
 			{
@@ -72,22 +72,22 @@ namespace NUtility
 			}
 
 			//------------------------------------------------------------------------
-			//					Compound Assignment Operators += -= *=
+			//		Compound Assignment Operators += -= *=
 			//------------------------------------------------------------------------
 			/**
 			* Note		These are destructive operators, because they update or replace the values 
-			*			on the left-hand side of the assignment. 
+			*		on the left-hand side of the assignment. 
 			*
-			*			We must have in mind that we might not want to do self-increment value for our class
-			*			eg. MyClass example; example += example;
-			*			For that we can check for equal instances
+			*		We must have in mind that we might not want to do self-increment value for our class
+			*		eg. MyClass example; example += example;
+			*		For that we can check for equal instances
 			*
 			* Signatures 
-			*			MyClass & MyClass::operator +=(const MyClass &rhs) 
-			*			MyClass & MyClass::operator -=(const MyClass &rhs)
-			*			MyClass & MyClass::operator *=(const MyClass &rhs)
+			*		MyClass & MyClass::operator +=(const MyClass &rhs) 
+			*		MyClass & MyClass::operator -=(const MyClass &rhs)
+			*		MyClass & MyClass::operator *=(const MyClass &rhs)
 			*
-			*			The operator returns a non-const reference to this
+			*		The operator returns a non-const reference to this
 			**/
 			CCopyableExample & CCopyableExample::operator+=(const CCopyableExample &rhs) 
 			{
@@ -106,14 +106,14 @@ namespace NUtility
 			//------------------------------------------------------------------------
 			/**
 			* Note		They don't modify either operand - they return a new value created from the two arguments.
-			*			For skipping extra work we can define them by using our compound assignment operators (if defined)
+			*		For skipping extra work we can define them by using our compound assignment operators (if defined)
 			*
 			* Signatures
-			*			const MyClass MyClass::operator+(const MyClass &other) const 
-			*			const MyClass MyClass::operator-(const MyClass &other) const
-			*			const MyClass MyClass::operator*(const MyClass &other) const
+			*		const MyClass MyClass::operator+(const MyClass &other) const 
+			*		const MyClass MyClass::operator-(const MyClass &other) const
+			*		const MyClass MyClass::operator*(const MyClass &other) const
 			*
-			*			The operator returns a new object.
+			*		The operator returns a new object.
 			**/
 			const CCopyableExample CCopyableExample::operator+(const CCopyableExample &other) const
 			{
@@ -124,22 +124,22 @@ namespace NUtility
 				// or return CCopyableExample(*this) += other;
 			}
 			//------------------------------------------------------------------------
-			//					Comparison and Relational Operators  
+			//			Comparison and Relational Operators  
 			//------------------------------------------------------------------------
 			/**
 			* Note		We return true if the objects are equal, based on our calculations in the overloaded operators
-			*			!= can and should be written in terms of the == operator
-			*			If you need one of the operators overloaded - overload both of them.
+			*		!= can and should be written in terms of the == operator
+			*		If you need one of the operators overloaded - overload both of them.
 			*
 			*
 			* Signatures
-			*			bool MyClass::operator == (const MyClass &other) const 
-			*			bool MyClass::operator != (const MyClass &other) const
+			*		bool MyClass::operator == (const MyClass &other) const 
+			*		bool MyClass::operator != (const MyClass &other) const
 			*
-			*			bool MyClass::operator  < (const MyClass& other) const
-			*			bool MyClass::operator <= (const MyClass& other) const
-			*			bool MyClass::operator >= (const MyClass& other) const
-			*			bool MyClass::operator  > (const MyClass& other) const
+			*		bool MyClass::operator  < (const MyClass& other) const
+			*		bool MyClass::operator <= (const MyClass& other) const
+			*		bool MyClass::operator >= (const MyClass& other) const
+			*		bool MyClass::operator  > (const MyClass& other) const
 
 			**/
 			bool CCopyableExample::operator==(const CCopyableExample &other) const 
@@ -151,18 +151,18 @@ namespace NUtility
 				return !(*this == other);
 			}
 			//------------------------------------------------------------------------
-			//					Input Output operators
+			//			Input Output operators
 			//------------------------------------------------------------------------
 			/**
 			* Note		It is important to make operator overloading function a friend of the class because 
-			*			it would be called without creating an object.
+			*		it would be called without creating an object.
 			*
 			* Signatures
-			*			friend ostream &operator<<(ostream &output, const MyClass &other)
-			*			friend istream &operator>>(istream &input, MyClass &other)
+			*		friend ostream &operator<<(ostream &output, const MyClass &other)
+			*		friend istream &operator>>(istream &input, MyClass &other)
 			*
-			*			Notice that the istream obj is not const because we need to change the data inside
-			*			We use friend to have access to the private members inside.
+			*		Notice that the istream obj is not const because we need to change the data inside
+			*		We use friend to have access to the private members inside.
 			**/
 			friend std::ostream &operator << (std::ostream &output, const CCopyableExample &obj)
 			{
@@ -180,21 +180,21 @@ namespace NUtility
 			//------------------------------------------------------------------------
 			/**
 			* Note		This example can be seen in my NContainersHelpers.hpp post / file
-			*			Github link: https://github.com/badearobert/cplusplus/
-			*			Blog post:   http://badearobert.ro/blog/2016/10/06/initmap-initvector-util-classes/
+			*		Github link: https://github.com/badearobert/cplusplus/
+			*		Blog post:   http://badearobert.ro/blog/2016/10/06/initmap-initvector-util-classes/
 			*
 			* Signatures
-			*			CCopyableExample& operator()(...)	
-			*			CCopyableExample  operator()(...)
+			*		CCopyableExample& operator()(...)	
+			*		CCopyableExample  operator()(...)
 			*		
-			*			Can be used with or without ref, depending on our implementation:
+			*		Can be used with or without ref, depending on our implementation:
 			*
-			*			1) 
-			*			We can create a new object inside the function, do something with the data received, and return
-			*			This can be done only with copy by value because the object was created locally.
+			*		1) 
+			*		We can create a new object inside the function, do something with the data received, and return
+			*		This can be done only with copy by value because the object was created locally.
 			*
-			*			2)
-			*			We can update the *this based on the data received, and return it as a reference
+			*		2)
+			*		We can update the *this based on the data received, and return it as a reference
 			**/
 			// overload function call
 			CCopyableExample& operator()(uint32_t value1, uint32_t value2)
@@ -203,7 +203,7 @@ namespace NUtility
 				return *this;
 			}
 			//------------------------------------------------------------------------
-			//					Subscripting operator []
+			//			Subscripting operator []
 			//------------------------------------------------------------------------
 			/**
 			* Note		
